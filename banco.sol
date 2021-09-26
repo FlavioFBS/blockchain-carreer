df006d7e7c9ad9e5989567de6f7e0f3650322d8c
FlavioFBS <f.fbs.95.ffbs@gmail.com>
2021-09-26 17:19:00 -0500

    transacctions
    setBalance on the samrt contract, transfer to the sender

---
+++ banco.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Banco {
+    
+    // CLASE: https://www.youtube.com/watch?v=G9SgweZ1U9I&list=PL-jD2YjEmwDpkSGIJ9tF4R1CM2TadvCRc&index=3
+    
+    // payable: para que la funcion reciba dinero
+    constructor() payable {
+        
+    }
+    
+    function incrementBalance (uint256 amount) payable public {
+        // se condiciona para que quien ejecuta la funcion, realmente envíe el valor que indica 
+        // ejm: si indico que enviaré 30 pero realmente envío un valor diferente, es un error y el estado del contrato no se actualiza
+        require(msg.value == amount);
+    }
+    
+    /* NOTA: El saldo que tenga el contrato es del contrato, no es del que lo
+        creó o hizo el contrato 
+        -Tiene un identidad autónoma en la blockchain
+    */
+    
+    function getBalance() public {
+        // msg.sender: direccion de origen de quien llama a la funcion dentro del contrato
+
+        // NOTA: mejor usar transfer que send, porque tiene unos problemas .-.
+        
+        // se le transferira Todo el balance del contrato a quien ejecuto el contrato
+        payable(msg.sender).transfer(address(this).balance);
+    }
+    
+    // funcion anonima: evitar usarla (°-°)  para evitar perdidas de dinero a los usuarios
+    function() payable public {
+        
+    }
+    
+}
